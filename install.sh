fd0f2083c04590ee0aedae41147a17cdc93c8f03
Brian Luft <brian@lincolnloop.com>
2012-12-20 10:22:45 -0800

    removes file linking, not needed

--- install.sh
+++ install.sh
@@ -1,31 +1,4 @@
-#!/usr/bin/env bash
-
-#credits:
-# sontek
-# yml
-
-function link_file {
-    source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
-
-    if [ -e "${target}" ]; then
-        mv $target $target.bak
-    fi
-
-    ln -sf ${source} ${target}
-}
-
-if [ "$1" = "vim" ]; then
-    for i in _vim*
-    do
-       link_file $i
-    done
-else
-    for i in _*
-    do
-        link_file $i
-    done
-fi
+#!/usr/bin/env sh
 
 git submodule sync
 git submodule init
